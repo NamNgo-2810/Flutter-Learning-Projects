42c631ada707c2771fc53c0737e47b986a61a967
unknown <henrynam28@gmail.com>
2022-03-17 20:43:30 +0700

    Work with API

---
+++ Clima-Flutter/lib/screens/location_screen.dart
@@ -0,0 +1,121 @@
+import 'package:clima/screens/city_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:clima/utilities/constants.dart';
+import '../services/weather.dart';
+
+class LocationScreen extends StatefulWidget {
+
+    final locationWeather;
+
+    LocationScreen(this.locationWeather);
+
+  @override
+    _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+    WeatherModel weather = WeatherModel();
+    double temperature;
+    String weatherIcon;
+    String cityName, message;
+    @override
+    void initState() {
+        super.initState();
+        updateUI(widget.locationWeather);
+    }
+
+    void updateUI(dynamic weatherData) {
+        try {
+            temperature = weatherData['main']['temp'];
+            weatherIcon = weather.getWeatherIcon(weatherData['weather'][0]['id']);
+            cityName = weatherData['name'];
+            message = weather.getMessage(temperature.round());
+        }
+        catch(e) {
+            print(e);
+        }
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            body: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('images/location_background.jpg'),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.white.withOpacity(0.8), BlendMode.dstATop),
+                    ),
+                ),
+                constraints: BoxConstraints.expand(),
+                child: SafeArea(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                            Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                    TextButton(
+                                        onPressed: () async {
+                                            var data = await weather.getLocationWeather();
+                                            updateUI(data);
+                                        },
+                                        child: Icon(
+                                            Icons.near_me,
+                                            size: 50.0,
+                                        ),
+                                    ),
+                                    TextButton(
+                                        onPressed: () async {
+                                            var typedName = await Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) {
+                                                        return CityScreen();
+                                                    }
+                                                ),
+                                            );
+                                            if (typedName != null) {
+                                                var data = await weather.getCityWeather(typedName);
+                                                updateUI(data);
+                                            }
+                                        },
+                                        child: Icon(
+                                            Icons.location_city,
+                                            size: 50.0,
+                                        ),
+                                    ),
+                                ],
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(left: 15.0),
+                                child: Row(
+                                    children: <Widget>[
+                                        Text(
+                                            '$temperature',
+                                            style: kTempTextStyle,
+                                        ),
+                                        Text(
+                                            weatherIcon,
+                                            style: kConditionTextStyle,
+                                        ),
+                                    ],
+                                ),
+                            ),
+                            Padding(
+                                padding: EdgeInsets.only(right: 15.0),
+                                child: Text(
+                                    message + " in " + cityName,
+                                    textAlign: TextAlign.right,
+                                    style: kMessageTextStyle,
+                                ),
+                            ),
+                        ],
+                    ),
+                ),
+            ),
+        );
+    }
+}
---
+++ Clima-Flutter/lib/services/networking.dart
@@ -0,0 +1,22 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class NetworkHelper {
+
+    final String url;
+
+    NetworkHelper(this.url);
+
+    Future getData() async {
+        try {
+            http.Response response = await http.get(Uri.parse(url));
+            if (response.statusCode == 200) {
+                return jsonDecode(response.body);;
+            }
+            else print(response.statusCode);
+        }
+        catch(e) {
+            print(e);
+        }
+    }
+}
